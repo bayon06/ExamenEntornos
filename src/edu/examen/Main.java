6d805d721587d6dc1c0f69e81f63da1b6c12f1f3
Presencial <Presencial@DESKTOP-UPE0222.mshome.net>
2025-12-05 13:24:35 +0100

    Main con depuración configurada

---
+++ src/edu/examen/Main.java
@@ -0,0 +1,10 @@
+package edu.examen;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Producto producto = new Producto("balón", 10);
+		System.out.print(producto.toString());
+	}
+
+}
